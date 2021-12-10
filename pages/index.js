314d6f7080ac08f8346997a551c8f98b196db749
lifeoncanvas <zoedalvi1902@gmail.com>
2021-12-10 09:50:08 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
-import { Container, Row, Col } from "reactstrap";
+
+
 
 import Dashboard from "../components/Navigation/Dashboard";
 
